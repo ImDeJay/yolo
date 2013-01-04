cbfccb5bd93dc60b2df4aee49a0f41c8107f17f7
DeJay Crawford <dejayyy@gmail.com>
2013-01-03 21:05:09 -0600

    removed unsave methods

--- src/com/gmail/dejayyy/yolo/yolo.java
+++ src/com/gmail/dejayyy/yolo/yolo.java
@@ -14,7 +14,7 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -51,8 +51,9 @@ public class yolo extends JavaPlugin implements Listener{
 	}
 	
 	
+	@SuppressWarnings("deprecation")
 	@EventHandler
-	public void playerChat(AsyncPlayerChatEvent event){
+	public void playerChat(PlayerChatEvent event){
 		
 		Player player = event.getPlayer();
 		String message = event.getMessage();
